48b6a3c8782c02a18bc42caee5abaf86b43ee602
jackonedev <jackone.action.software@gmail.com>
2024-07-01 17:54:05 -0300

    update utils.config: routing the static path from the config path

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,13 @@
 import os
 
+
+# Obtain current file path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Obtain the relative path to the static directory from the current file
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
 # Create a list of each of the image files in the static/img directory
-img_dir = "./static/img"
+img_dir = f"{STATIC_DIR}/img"
 img_files = os.listdir(img_dir)
 IMG_ROUTES = [os.path.join(img_dir, img_file) for img_file in img_files]
