ba8d07da2a6a6dc28f934bf756e0850d015688b9
jackonedev <jackone.action.software@gmail.com>
2024-07-02 17:55:45 -0300

    move function: from image_utils to api/tools

--- ocr_service/ocrlogic/image_utils.py
+++ ocr_service/ocrlogic/image_utils.py
@@ -1,18 +1,6 @@
-import os
-import shutil
 from contextlib import contextmanager
 
 from utils.config import IMG_ROUTES
-
-
-def _save_file_to_server(uploaded_file, path=".", save_as="default"):
-    extension = os.path.splitext(uploaded_file.filename)[-1]
-    temp_file = os.path.join(path, save_as + extension)
-
-    with open(temp_file, "wb") as buffer:
-        shutil.copyfileobj(uploaded_file.file, buffer)
-
-    return temp_file
 
 
 def _search_img(img_path):
