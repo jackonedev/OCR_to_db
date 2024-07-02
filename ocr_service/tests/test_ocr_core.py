24539c770c1afa451eb3848fed7ac886e6cdbb6c
Agustín F. Stigliano <113382260+jackonedev@users.noreply.github.com>
2024-07-02 14:33:51 +0000

    fix ocr_service/tests with async functions

--- ocr_service/tests/test_ocr_core.py
+++ ocr_service/tests/test_ocr_core.py
@@ -2,19 +2,19 @@ import pytest
 
 from ocrlogic.ocr_core import ocr_core
 
-
-def test_ocrcore_supported_lang_param():
+@pytest.mark.asyncio
+async def test_ocrcore_supported_lang_param():
     img_path = "gran_registro"
     lang = "eng"
-    result = ocr_core(img_path=img_path, lang=lang)
+    result = await ocr_core(img_path=img_path, lang=lang)
     assert isinstance(result, str), "Should return a string"
 
-
-def test_ocrcore_invalid_lang_param():
+@pytest.mark.asyncio
+async def test_ocrcore_invalid_lang_param():
     img_path = "gran_registro"
     lang = "ger"
     with pytest.raises(ValueError) as e:
-        ocr_core(img_path=img_path, lang=lang)
+        await ocr_core(img_path=img_path, lang=lang)
     assert (
         str(e.value)
         == f"Language {lang} not supported. Please choose between 'eng' or 'spa'."
