ba7ae841eb2a5e35f2e733b6a93bc2a71b8f7e66
jackonedev <jackone.action.software@gmail.com>
2024-07-05 08:38:02 -0300

    updated ocr_service: main.py CORS and redirect

--- ocr_service/main.py
+++ ocr_service/main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from api import service
 
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+)
 
 app.include_router(service.router)
 
-
 @app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+@app.get("/check")
 async def greetings():
     return {"message": "Hola mundo"}
